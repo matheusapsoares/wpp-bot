bf63b1cbcf1275d356ee2f2cbaedc4b85095c6f8
Matheus Santos <matheus.apsoares@outlook.com>
2023-01-05 18:11:33 -0300

    Apagando codigos nao utilizados

--- src/indicators/indicators.controller.ts
+++ src/indicators/indicators.controller.ts
@@ -1,19 +1,11 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { IndicatorsService } from './indicators.service';
-import { CreateIndicatorDto } from './dto/create-indicator.dto';
-import { UpdateIndicatorDto } from './dto/update-indicator.dto';
 import { WppClientService } from 'src/WppClient.service';
 
 @Controller('indicators')
 export class IndicatorsController {
   constructor(private readonly indicatorsService: IndicatorsService) {
     //const clientWpp = new WppClientService
-  }
-
-  @Post('sender')
-  create(@Body() createIndicatorDto: CreateIndicatorDto) {
-    
-    //return this.indicatorsService.create(createIndicatorDto);
   }
 
   @Get()
